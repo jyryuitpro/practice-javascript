e296c0c68fae3f4d81320e59a83e7853dc753970
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-16 19:42:43 +0900

    1-4. 비교 연산자

---
+++ 1-4. 비교 연산자.js
@@ -0,0 +1,19 @@
+console.log(NaN);
+console.log('Not a Number');
+console.log(NaN == NaN);
+console.log(1 == '1');
+5 == '5'
+5 === '5'
+NaN == NaN
+5 == '5'
+5 == '5'
+5 === '5'
+5 === 5
+NaN === NaN
+5 != 5
+5 != '5'
+5 !== '5'
+5 === 5
+5 !== 5
+5 !== 6
+5 ===6
