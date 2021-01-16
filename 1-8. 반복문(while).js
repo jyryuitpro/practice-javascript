046d35ff111caa57e3ff7406a8eaee39761374bf
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-16 21:23:08 +0900

    1-8. 반복문(while)

---
+++ 1-8. 반복문(while).js
@@ -0,0 +1,45 @@
+'안녕하세요'
+'안녕하세요'
+'안녕하세요'
+'안녕하세요'
+'안녕하세요'
+
+console.log('안녕하세요');
+console.log('안녕하세요');
+console.log('안녕하세요');
+console.log('안녕하세요');
+console.log('안녕하세요');
+
+var 결과 = 1
+결과 = 결과 + 2
+결과 = 결과 + 3
+결과 = 결과 + 4
+결과 = 결과 + 5
+
+// if
+if (조건) {
+
+} else {
+
+}
+
+// while for
+while (false) {
+    console.log('안녕하세요');
+}
+
+while (true) {
+    console.log('안녕하세요');
+}
+
+var 값 = 0;
+while (값 < 100) {
+    console.log(값);
+    값 = 값 + 1;
+}
+
+var 값 = 0;
+while (값 < 5) {
+    console.log('안녕하세요');
+    값 = 값 + 1;
+}
