32da0f8b547fd1f82b3b94dcba400c6646db460a
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-16 22:20:28 +0900

    1-10. 함수

---
+++ 1-10. 함수.js
@@ -0,0 +1,37 @@
+String(5);
+
+3 ** 3
+
+function 함수(x, z) {
+    return x + z + 5;
+}
+
+함수(3, 2);
+
+function 더하기(첫째, 둘째) {
+    return 첫째 + 둘째 + 3 + 5;
+}
+
+더하기(2, 8);
+더하기(4, 7);
+더하기(8, 1);
+
+console.log('안녕하세요.');
+console.log('류지영A님');
+console.log('반가워요');
+
+console.log('안녕하세요.');
+console.log('류지영B님');
+console.log('반가워요');
+
+console.log('안녕하세요.');
+console.log('류지영C님');
+console.log('반가워요');
+
+function 인사(이름) {
+    console.log('안녕하세요.');
+    console.log(이름 + '님');
+    console.log('반가워요');
+}
+
+인사('류지영');
