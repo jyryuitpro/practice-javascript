8fe454c96063dfd705b44ee7275e170ea18f27cd
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-16 20:05:58 +0900

    1-5. 변수(var)

---
+++ 1-5. 변수(var).js
@@ -0,0 +1,44 @@
+console.log(1 * 5);
+console.log(5 === 5);
+console.log(1 * 2);
+console.log(123 * 246);
+console.log(30258 * 467);
+console.log(14120486 * 234);
+console.log(3304193724 / 6);
+console.log(550698954 * 3);
+console.log(3 * 5);
+var 결과 = 15;
+console.log('배리어블');
+console.log('variable');
+console.log('변수');
+console.log(1 + 2);
+console.log(결과);
+var 이름 = '류지영'
+이름
+var 참거짓 = false
+결과
+이름
+참거짓
+3 * 5
+15
+var 결과 = 15;
+var 결과 = 3 * 6;
+var result = 15;
+var result = 15;
+var 이름 = '류지영';
+var name = '류지영';
+var name2 = '네로';
+var name_three = '아아';
+// var #$% = 'asdf';
+var $ = '달러';
+var _ = '언더바';
+// var !@#%^&*()+
+// var 2adfaf = 'adf';
+// var (112312) = '안녕';
+var $ = '달러';
+// var 22안녕 = '안돼!';
+var 결과 = 3 * 5;
+결과 === 15
+결과 === 14
+var 결과 = 5 * 4;
+결과 === 20
