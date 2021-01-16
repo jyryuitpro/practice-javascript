e1ba88c8ceb715dbea0eedbdf5330a61441bdf12
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-16 21:03:28 +0900

    1-7. 조건문(if)

---
+++ 1-7. 조건문(if).js
@@ -0,0 +1,46 @@
+var 조건 = 5 * 5 === 25;
+
+if (조건) {
+    '딩동댕'
+} else {
+    '땡'
+}
+
+if (false) {
+    '딩동댕'
+} else {
+    '땡'
+}
+
+if (5 * 5 === 25) {
+    '딩동댕'
+} else {
+    '땡'
+}
+
+if (3 * 8 === prompt('답?')) {
+    '딩동댕'
+} else {
+    '땡'
+}
+
+prompt('답?');
+
+Number(prompt('답?'));
+
+var 답 = prompt('답?');
+Number(답);
+
+var 답 = prompt('답?');
+if (3 * 8 === Number(답)) {
+    '딩동댕'
+} else {
+    '땡'
+}
+
+var 답 = prompt('답?');
+if (3 * 8 === Number(답)) {
+    '딩동댕'
+} else {
+    '땡'
+}
