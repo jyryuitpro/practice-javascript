1a8584c5fd792970449db278562b20afb76bb32f
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-16 19:10:30 +0900

    1-3. 연산자

---
+++ 1-3. 연산자.js
@@ -0,0 +1,32 @@
+console.log(5);
+console.log(1 + 2);
+console.log(5);
+console.log(1 + 2);
+console.log(123456789 * 123456789);
+console.log(3 * 5);
+console.log(10 / 2);
+console.log(9 / 3);
+console.log(10 / 3);
+console.log(11 / 3);
+console.log(10 - 3);
+console.log(10 - 5);
+console.log('문자' - 5);
+console.log(NaN);
+console.log('연산자');
+console.log(5 > 3);
+console.log(5 < 3);
+console.log('문자' < 5);
+console.log(5 >= 5);
+console.log(6 <= 4);
+console.log(10 >= 3);
+console.log(5 == 5);
+console.log(4 == 4);
+console.log('문자' == '문자');
+console.log(false == false);
+console.log(1 * 5);
+console.log(5 == 5);
+console.log(5 == 4);
+// 5 == 5
+// 5 = 5
+
+
