e74f250b85c53e073d1a76d63b5ce4ac049c186c
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-21 07:23:18 +0900

    3-3. script 태그 사용법

---
+++ 3-3. script 태그 사용법.js
@@ -0,0 +1,10 @@
+var word = '류지영';
+while (true) {
+    var answer = prompt(word);
+    if (word[word.length - 1] === answer[0]) {
+        word = answer;
+        alert('딩동댕');
+    } else {
+        alert('땡');
+    }
+}
