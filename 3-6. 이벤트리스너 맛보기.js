c3c532ee673e7101ea7128ac9f8f98e457ab42af
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-24 21:42:02 +0900

    3-6. 이벤트리스너 맛보기

---
+++ 3-6. 이벤트리스너 맛보기.js
@@ -0,0 +1,43 @@
+var 바디 = document.body;
+
+var 단어 = document.createElement('div');
+단어.textContent = '류지영';
+document.body.append(단어);
+
+var 폼 = document.createElement('form');
+document.body.append(폼);
+
+var 입력창 = document.createElement('input');
+폼.append(입력창);
+
+var 버튼 = document.createElement('button');
+버튼.textContent = '입력!';
+폼.append(버튼);
+
+var 결과창 = document.createElement('div');
+document.body.append(결과창);
+
+폼.addEventListener('submit', function 콜백함수(e) { // 콜백함수, 익명함수
+    e.preventDefault();
+    if (단어.textContent[단어.textContent.length - 1] === 입력창.value[0]) {
+        결과창.textContent = '딩동댕';
+        단어.textContent = 입력창.value;
+        입력창.value = '';
+        입력창.focus();
+    } else {
+        결과창.textContent = '땡';
+        입력창.value = '';
+        입력창.focus();
+    }
+});
+
+// var word = '류지영';
+// while (true) {
+//     var answer = prompt(word);
+//     if (word[word.length - 1] === answer[0]) {
+//         word = answer;
+//         alert('딩동댕');
+//     } else {
+//         alert('땡');
+//     }
+// }
